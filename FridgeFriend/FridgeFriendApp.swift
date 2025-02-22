0a41ea232024bb33fe0b2d66069a5653ad966170
Colin <danforthc@wit.edu>
2025-02-22 17:57:33 -0500

    Initial Commit

---
+++ FridgeFriend/FridgeFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeFriendApp.swift
+//  FridgeFriend
+//
+//  Created by Colin James on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FridgeFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeFriendTests/FridgeFriendTests.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeFriendTests.swift
+//  FridgeFriendTests
+//
+//  Created by Colin James on 2/22/25.
+//
+
+import Testing
+@testable import FridgeFriend
+
+struct FridgeFriendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
