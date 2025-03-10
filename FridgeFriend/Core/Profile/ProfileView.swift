73e5af46edd9a2d63ae49d1b5f855b186168df82
Colin <danforthc@wit.edu>
2025-03-09 23:38:15 -0400

    added firebase UI and implemented authentication

--- FridgeFriend/Core/Profile/ProfileView.swift
+++ FridgeFriend/Core/Profile/ProfileView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        Text("Hello, Profile!")
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(Circle())
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.name)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                Section("Account") {
+                    Button {
+                        viewModel.signOut()
+                    } label: {
+                        SettingRowView(imageName: "arrow.left.circle.fill",
+                                       title: "Sign Out",
+                                       tintColor: Color(.systemRed))
+                    }
+                    
+                    
+                    Button {
+                        print("Delete Account")
+                    } label: {
+                        SettingRowView(imageName: "xmark.circle.fill",
+                                       title: "Delete Account",
+                                       tintColor: Color(.systemRed))
+                    }
+                }
+            }
+        }
     }
 }
 
