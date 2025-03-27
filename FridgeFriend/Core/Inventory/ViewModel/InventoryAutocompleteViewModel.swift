0de2dd95c6f4b5ca1d19e251b6c1ff319c72242b
Colin <danforthc@wit.edu>
2025-03-27 17:04:36 -0400

    provisioning profile plus some bare bones files for api implementation

---
+++ FridgeFriend/Core/Inventory/ViewModel/InventoryAutocompleteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  InventoryAutocompleteViewModel.swift
+//  FridgeFriend
+//
+//  Created by Colin James on 3/26/25.
+//
+
+import Foundation
---
+++ FridgeFriend/Services/InventoryAutocomplete.swift
@@ -0,0 +1,8 @@
+//
+//  InventoryAutocomplete.swift
+//  FridgeFriend
+//
+//  Created by Colin James on 3/26/25.
+//
+
+import Foundation
